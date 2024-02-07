cea8de060ca25283d58f08678df5f09764f46e5b
KAYLANE MAFRA STRIZZI <kaylane_strizzi@estudante.sesisenai.org.br>
2024-02-07 17:34:49 -0300

    primeira versao

---
+++ temperatura.js
@@ -0,0 +1,15 @@
+let resposta = document.getElementById('resposta')
+function calcular(){
+  let graus =  Number (document.getElementById('graus').value)
+  let cleitin = (graus * 1.8) + 32
+  resposta.innerHTML = "O valor em graus é:" + cleitin
+}
+
+
+
+    
+
+
+
+
+
